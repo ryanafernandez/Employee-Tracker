41b657641b51a0a3bc2313afee44a03badea07f8
Ryan Fernandez <ryanfernandez11@gmail.com>
2022-12-21 14:43:01 -0800

    deleted employee.js department.js and role.js from src file and conslidated inquirer flow into index.js due to issue with displaying data when using imported functions with inquirer.

--- index.js
+++ index.js
@@ -1,98 +1,185 @@
-const express = require('express');
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+require('console.table');
+const connection = require('./config/connection');
 
-const { menuPrompt } = require('./src/questions');
-const { viewAllEmployees, addEmployee, deleteEmployee } = require('./src/employee');
-const { updateRole, addRole, deleteRole } = require('./src/role');
-const { viewAllDepartments, addDepartment, deleteDepartment } = require('./src/departments');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'employees_db'
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
-});
+const { menuPrompt, addEmployeePrompt, addRolePrompt, updateRolePrompt1, updateRolePrompt2, addDeptPrompt } = require('./src/questions');
 
 // Presents Title Screen and User Menu
 async function init() {
-    
     // Present title screen
     console.log("Presenting menu screen");
     mainMenu();
 }
-
+                
 // Runs mainMenu prompt and calls the corresponding function based on user choice
-function mainMenu() {
-    inquirer.prompt(menuPrompt).then((answers) => {
+async function mainMenu() {
+    await inquirer.prompt(menuPrompt).then((answers) => {
         switch(answers.menuPrompt) {
             case 'View All Employees':
                 connection.promise().query('SELECT * FROM employee')
-                    .then ( ([rows, fields]) => {
-                        viewAllEmployees(rows);
+                    .then( ([rows, fields]) => {
+                        console.log('\n\n');
+                        console.table(rows);
                         mainMenu();
-                    })
-                    .catch(console.log)
-                    .then( () => connection.end());
-                
+                    });
                 break;
             case 'Add Employee':
                 addEmployee();
                 break;
-            case 'Delete Employee':
-                deleteEmployee();
-                break;
-            case 'Update Employee Role':
-                updateRole();
+            case 'View All Roles':
+                connection.promise().query('SELECT * FROM role')
+                    .then( ([rows, fields]) => {
+                        console.log('\n\n');
+                        console.table(rows);
+                        mainMenu();
+                    });
                 break;
             case 'Add Role':
                 addRole();
                 break;
-            case 'Delete Role':
-                deleteRole();
+            case 'Update Employee Role':
+                updateEmployeeRole();
                 break;
             case 'View All Departments':
-                viewAllDepartments();
+                connection.promise().query('SELECT * FROM department')
+                    .then( ([rows, fields]) => {
+                        console.log('\n\n');
+                        console.table(rows);
+                        mainMenu();
+                    });
                 break;
             case 'Add Department':
                 addDepartment();
                 break;
-            case 'Delete Department':
-                deleteDepartment();
-                break;
-            case 'Update Employees By Managers':
-                // select manager
-                // select employee
-                // select column to update
-                // edit more?
-                break;
-            case 'View Employees By Manager':
-                // select manager
-                break;
-            case 'View Budget By Department':
-                // select department
-                break;
             case 'Quit':
                 console.log("Quitting application");
+                connection.end();
                 return;
             default:
                 console.log("Selecting default option...");
+                connection.end();
                 return;
         }
-        
+    });
+}
+
+async function addEmployee() {
+    await inquirer.prompt(addEmployeePrompt).then((answers) => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)`;
+        const params = [ answers.first_name, answers.last_name, answers.role_id, answers.manager_id ];
+        connection.promise().query(sql, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+        })
+        mainMenu();
+    });
+}
+
+async function addRole() {
+    await inquirer.prompt(addRolePrompt).then((answers) => {
+        let title = answers.title;
+        let salary = answers.salary;
+        let department_id;
+        switch (answers.department) {
+            case 'Sales':
+                department_id = 1;
+                break;
+            case 'Engineering':
+                department_id = 2;
+                break;
+            case 'IT':
+                department_id = 3;
+                break;
+            default:
+                department_id = 1;
+        }
+
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES(?, ?, ?)`;
+        const params = [ title, salary, department_id];
+        connection.promise().query(sql, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+        });
+        mainMenu();
+    });
+}
+
+async function addDepartment() {
+    await inquirer.prompt(addDeptPrompt).then((answers) => {
+        const sql = `INSERT INTO department (name) VALUES(?)`;
+        const params = answers.name;
+        connection.promise().query(sql, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+        });
+        mainMenu();
+    });
+}
+
+async function updateEmployeeRole() {
+    const names = [];
+    const roles = [];
+
+    const sqlNames = `SELECT id, CONCAT(first_name, \' \', last_name) AS name FROM employee`;
+    const sqlRoles = `SELECT id, title FROM role`;
+    const sqlUpdate = `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+    // Run sqlNames query to pull all employee names and store into names array
+    connection.promise().query(sqlNames)
+    .then( ([rows, fields]) => {
+        rows.forEach(row => {
+            names.push(row.name);
+        });
+        // Prompt the user to select which employee they would like to update
+        inquirer
+            .prompt({
+                type: 'list',
+                message:'Which employee would you like to update?',
+                name: 'name',
+                choices: names,
+            })
+            .then((employee) => {
+                // Find the employee's id in the employee table
+                const id = names.findIndex(element => element == employee.name) + 1;
+
+                // Run sqlRoles query to pull all roles and store into roles array
+                connection.promise().query(sqlRoles)
+                .then( ([rows, fields]) => {
+                    rows.forEach(row => {
+                        roles.push(row.title);
+                    })
+                    // Prompt the user to select what the employee's new role is
+                    inquirer.prompt({
+                        type: 'list',
+                        name: 'newRole',
+                        message: 'What is the employee\'s new role?',
+                        choices: roles,
+                    })
+                    .then((role) => {
+                        // Find the corresponding role_id in the role table
+                        const roleId = roles.findIndex(element => element == role.newRole) + 1;
+                        const params = [ roleId, id ];
+
+                        // Run sqlUpdate query to update the employee table
+                        connection.promise().query(sqlUpdate, params, (err, result) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            console.log(result);
+                        });
+
+                        // Return to the main menu
+                        mainMenu();
+                    });
+                });
+            });  
     });
 }
 
 init();
-
-module.exports = {
-    connection,
-    mysql
-};
--- src/questions.js
+++ src/questions.js
@@ -6,22 +6,107 @@ const menuPrompt = [
         choices: [
             'View All Employees',
             'Add Employee',
-            'Delete Employee',
-            'Update Employee Role',
+            // 'Delete Employee',
+            'View All Roles',
             'Add Role',
-            'Delete Role',
+            'Update Employee Role',
+            // 'Delete Role',
             'View All Departments',
             'Add Department',
-            'Delete Department',
-            'Update Employees By Managers',
-            'View Employees By Manager',
-            'Delete Department, Roles, and Employees',
-            'View Budget By Department',
+            // 'Delete Department',
+            // 'Update Employees By Managers',
+            // 'View Employees By Manager',
+            // 'View Budget By Department',
             'Quit'
         ],
     },
 ];
 
+const addEmployeePrompt = [
+    {
+        type: 'input',
+        message: 'What is the employee\'s first name?',
+        name: 'first_name', 
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s last name?',
+        name: 'last_name',
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s role id?',
+        name: 'role_id',
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s manager id?',
+        name: 'manager_id',
+    },
+];
+
+const addRolePrompt = [
+    {
+        type: 'input',
+        message: 'What is the title of the role you would like to add?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the role\'s salary?',
+        name: 'salary',
+    },
+    {
+        type: 'list',
+        message: 'What department does this role belong to?',
+        name: 'department',
+        choices: [
+            'Sales',
+            'Engineering',
+            'IT'
+        ],
+    },
+];
+
+const updateRolePrompt1 = [
+    {
+        type: 'input',
+        message: 'What is the role id of the role you would like to update?',
+        name: 'id',
+    }
+];
+
+const updateRolePrompt2 = [
+    {
+        type: 'input',
+        message: 'Enter the new role title (press \'Enter\' if unchanged)',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter the new role salary (press \'Enter\' if unchanged)',
+        name: 'salary',
+    },
+    {
+        type: 'input',
+        message: 'Enter the new role department id (press \'Enter\' if unchanged)',
+        name: 'department_id',
+    },
+];
+
+const addDeptPrompt = [
+    {
+        type: 'input',
+        message: 'What is the name of the department you would like to add?',
+        name: 'name',
+    }
+];
+
 module.exports = {
-    menuPrompt
+    menuPrompt,
+    addRolePrompt,
+    addDeptPrompt,
+    addEmployeePrompt,
+    updateRolePrompt1,
+    updateRolePrompt2
 };
